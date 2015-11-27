5fe12c4827eeba5fd7a9f6426d632ed57f38e817
pl12133 <krirken@gmail.com>
2015-11-26 19:37:40 -0500

    patchAllAvailableErrorsInLogFile now returns a patchInfo object containing information about the fixes that were performed

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 
 let { getFileLines, openFileForPatch, overwriteFileWithLines, getPatchCount } = require('./fsUtils');
 let { default: getErrorsOfLogFile } = require('./lintLogParser');
+let { default: beginInputListening } = require('./inputListener');
 // 1. Load lint log.
 //
 // 2. Split into individual files.
@@ -13,24 +14,34 @@ let args = process.argv.slice(2);
 let logFileName = args[0];
 if (!logFileName) {
   let { argv } = process;
-  console.log(`Usage: node autoLintFixer.js [es-lint-log]`);
+  console.log(`Usage: babel-node -- autoLintFixer.js [es-lint-log]`);
   process.exit(1);
 }
 
-
-
 function patchAllAvailableErrorsInLogFile(logFileName) {
+  // This function should return a patchInfo object
   let errorInfo = getErrorsOfLogFile(logFileName);
+  let batchPatchInfo = {
+    totalErrorCount: 0,
+    patches: []
+  };
   errorInfo.forEach((info, index) => {
-    let patchedFileLines = openFileForPatch(info);
-    //overwriteFileWithLines(info.file, patchedFileLines);
-    //writeFileLinesWithPostfix(info.file, patchedFileLines);
+    let patchInfo = openFileForPatch(info);
+    let patchedFileLines = patchInfo.lines;
+    // File overwriting is currently disabled during development 
+    // overwriteFileWithLines(info.file, patchedFileLines);
+    // writeFileLinesWithPostfix(info.file, patchedFileLines);
+
+    batchPatchInfo.patches.push(patchInfo);
   });
+
+  return batchPatchInfo;
 }
 
 
 
 
-patchAllAvailableErrorsInLogFile(logFileName);
-console.log(`Total Patched: ${getPatchCount()}`);
+let batchPatchInfo = patchAllAvailableErrorsInLogFile(logFileName);
+console.log(`Patched ${getPatchCount()} of ${batchPatchInfo.patches.length} total errors`);
+beginInputListening(batchPatchInfo);
 module.exports.default = patchAllAvailableErrorsInLogFile;
